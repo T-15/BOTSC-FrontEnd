e4ebde49e58122450bc3f60359ad908b1b7ca735
James Tinker <j.tinker30@hotmail.com>
2020-05-26 13:09:56 -0400

    Added Positions Index

---
+++ src/private_views/positions/Index.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { useAuth0 } from "../../react-auth0-spa";
+import axios from 'axios';
+import TableWithHeader from "../../components/tables/TableWithHeader";
+
+function Index(props) {
+    const [positions, setPositions] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const { getTokenSilently } = useAuth0();
+    const url = process.env.REACT_APP_API_URL + 'private/positions';
+    const tableTitle = "Positions";
+
+    useEffect(() => {
+        async function fetchData() {
+            setIsError(false);
+            setIsLoading(true);
+            try {               
+                const token = await getTokenSilently();
+            
+                const result = await axios(
+                    url,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }
+                );
+            
+                const responseData = await result.data;
+                setIsLoading(false);
+                setPositions(responseData);
+            } catch (error) {
+                setIsLoading(false);
+                setIsError(true);
+            }
+        }
+        fetchData();
+    }, [url, getTokenSilently]);
+
+    return ( 
+        <div className="container">
+            {isLoading ? (
+                <div>Loading ...</div>
+            ) : (
+                <>
+                    {isError ? (
+                        <div>Something went wrong ...</div>
+                    ) : (
+                        <div>
+                            <TableWithHeader 
+                                title={tableTitle}
+                                items={positions}
+                            />
+                        </div>
+                    )}
+                </>
+            )}    
+            {props.children}
+        </div>
+    );
+}
+ 
+export default Index;
